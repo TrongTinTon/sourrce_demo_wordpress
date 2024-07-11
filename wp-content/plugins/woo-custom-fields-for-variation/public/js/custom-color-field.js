78b94697c0106bdfed93f0ea0f4f3c2fa9f7d123
TrongTinTon <128878654+TrongTinTon@users.noreply.github.com>
2024-07-12 00:33:59 +0700

    custom-color-input
    Hiệu chỉnh Element  Colorr Input
    Thêm icon, điều chỉnh css
    Thêm JS cho nhiều input color

--- wp-content/plugins/woo-custom-fields-for-variation/public/js/custom-color-field.js
+++ wp-content/plugins/woo-custom-fields-for-variation/public/js/custom-color-field.js
@@ -3,9 +3,10 @@ this.jscolor.presets.default = {
 		'#000000', '#7d7d7d', '#870014', '#ec1c23', '#ff7e26', '#fef100', '#22b14b', '#00a1e7', '#3f47cc', '#a349a4',
 		'#ffffff', '#c3c3c3', '#b87957', '#feaec9', '#ffc80d', '#eee3af', '#b5e61d', '#99d9ea', '#7092be', '#c8bfe7',
 	],
+
 	//paletteCols: 12,
-	//hideOnPaletteClick: true,
-	//width: 271,
+	hideOnPaletteClick: false,
+	width: 271,
 	//height: 151,
 	//position: 'right',
 	//previewPosition: 'right',
@@ -13,16 +14,27 @@ this.jscolor.presets.default = {
 }
 
 
-jQuery('.chose-color-press').addClass('jscolor');
 
 function updateBackground(el, selector) {
-	document.querySelector(selector).style.background = el.jscolor.toBackground()
+	document.querySelector(selector).style.background = el.jscolor.toRGBAString()
 }
 function updateValue(el, selector) {
 	jQuery(selector).prop('value',el.jscolor.toString()).change()
-
-
 }
+
+   
+document.querySelectorAll('.color-input-label').forEach(function(label){
+	label.addEventListener('mousedown', function(event) {
+	    event.stopPropagation(); // Ngăn chặn sự kiện click trên SVG
+		    var inputEle = document.getElementById(this.getAttribute('data-input'))
+		    inputEle.jscolor.show()
+		    console.log(inputEle)
+		    // parentElement.jscolor.show(); // Gọi sự kiện click trên thẻ cha
+	});
+})
+
+
+
 // triggers 'onInput' and 'onChange' on all color pickers when they are ready
 jscolor.trigger('input change');
 
--- wp-content/plugins/woo-custom-fields-for-variation/public/template/custom_color_field.php
+++ wp-content/plugins/woo-custom-fields-for-variation/public/template/custom_color_field.php
@@ -11,7 +11,7 @@ if(isset($_POST['custom-variation'][sanitize_title( $options['name'] )])){
 	$entered_data = str_replace('\"','"',$entered_data);
 	$entered_data = str_replace("\'","'",$entered_data);	
 } ?>
-<p class="phoen_minss phoenwwe_<?php echo $variation_id; ?> form-row form-row-wide custom_<?php echo sanitize_title( $options['name'] ); ?>" style="display:none;" >
+<div class="wrapper-color-input-element phoen_minss phoenwwe_<?php echo $variation_id; ?> form-row form-row-wide custom_<?php echo sanitize_title( $options['name'] ); ?>" style="display:none;" >
 	<?php if ( ! empty( $options['label'] ) ) :
 		$show  = "<label>";
 		$show .= wptexturize( $options['label'] ) . ' ' . $price; 
@@ -21,10 +21,59 @@ if(isset($_POST['custom-variation'][sanitize_title( $options['name'] )])){
 	<?= _e($show) ?>
 
 	
+	 <div class="color-input-wrapper">
+   	<div class="color-input-container">
+   		   <label class="color-input-label" data-input="<?php echo $variation_id.'-'.$option_name; ?>-remote-input">
+   		   	<img src="<?php echo plugin_dir_url( __FILE__ )."../image/paint.svg" ?>">
 
-	<input id="color-input-<?php echo $variation_id.'-'.$option_name; ?>" type="text" class="input-text custom-variation custom_color_field" data-price="<?php echo $options['price'] ? $options['price'] : '0'; ?>" name="custom-variation[<?php echo $variation_id; ?>][<?php echo sanitize_title( $option_name ); ?>]" value="<?php if( ! empty($entered_data) ){ echo $entered_data; } ?>" <?php if ( ! empty( $options['max'] ) ) echo 'maxlength="' . $options['max'] .'"'; ?>  />
+   		   </label>
+   		   <div>
+   		   	<input type="text" id="<?php echo $variation_id.'-'.$option_name; ?>-remote-input" class="color-input-text" data-jscolor="{previewElement:'#set-bg-<?php echo $variation_id.'-'.$option_name; ?>', value: '#e6e6e6'}"  onChange="updateValue(this,'#color-input-<?php echo $variation_id.'-'.$option_name; ?>')" onInput="updateBackground(this,'#set-bg-<?php echo $variation_id.'-'.$option_name; ?>')"/>
+   		   </div>
+   		  	
+   		   <div class="color-review-bg" id="set-bg-<?php echo $variation_id.'-'.$option_name; ?>" ></div>
+
+   	</div>
+
+	</div>
 	
-	<input class="chose-color-press" value="#e6e6e6" data-jscolor="{alpha:0.7}" onChange="updateValue(this,'#color-input-<?php echo $variation_id.'-'.$option_name; ?>')" onInput="updateBackground(this,'#set-bg-<?php echo $variation_id.'-'.$option_name; ?>')">
-	<em id="set-bg-<?php echo $variation_id.'-'.$option_name; ?>" style="display:inline-block; padding:1em;background:#e6e6e6;">input event pr4</em>
-</p>
+	<input type="hidden" id="color-input-<?php echo $variation_id.'-'.$option_name; ?>" type="text" class="input-text custom-variation custom_color_field" data-price="<?php echo $options['price'] ? $options['price'] : '0'; ?>" name="custom-variation[<?php echo $variation_id; ?>][<?php echo sanitize_title( $option_name ); ?>]" value="<?php if( ! empty($entered_data) ){ echo $entered_data; } ?>" <?php if ( ! empty( $options['max'] ) ) echo 'maxlength="' . $options['max'] .'"'; ?>  />
+
+  
+</div>
+<style>
+	.color-input-wrapper {
+	
+	}
+	.color-input-container {
+
+			display: flex;
+    gap: 8px;
+	}
+	.color-input-container .color-input-text {
+		width: 70px;
+    height: 25px;
+    padding: 0;
+    margin: 0;
+	}
+	.color-review-bg {
+		display:inline-block;background:#e6e6e6;
+	}
+	.color-review-bg {
+
+    min-width: 3em;
+    height: 1.5em;
+    border-radius: 4px
+	}
+	.color-input-label {
+		cursor: pointer;
+		margin: 0;
+	}
+	.color-input-label:active, .color-input-label svg:active {
+		 transform: scale(1.03);
+		 transition: all 0.2s ease;
+	}
+
+
+</style>
 
